836b840c5727563689da599d532928844f00b108
khansConstant <104765533+khansConstant@users.noreply.github.com>
2023-03-17 02:35:30 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
+root.render(<App />);
+
